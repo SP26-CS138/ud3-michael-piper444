261722252b37a8d6a3fceb2e788146f8b09a6cf6
Michael J Piper <michaelpiper444@gmail.com>
2026-04-22 03:07:26 +0000

    UD3_in_progress

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 '''
-DEVELOPER(S): <your name(s)>
+DEVELOPER(S): <Michael Piper>
 COLLABORATORS: <anyone who helped you>
-DATE: <date last worked on>
+DATE: <04/20/2026>
 '''
 
 """
-A one line summary of the program, terminated by a period.
+Luiseno flashcard quiz program for language revitalization efforts.
 
-Leave one blank line.  The rest of this docstring should contain an
-overall description of the program.
+The program quizzes user on several different phrases in Luiseno language
 """
 
 ##########################################
 # IMPORTS:
-# <list of modules needed for the program and their purpose>
 ##########################################
-# <replace this line with import statement(s)>
+import random
 
 
 ##########################################
 # FUNCTIONS:
 ##########################################
 # <replace this line with function definitions, each needs a description>
+def create_file():
+    with open("Luiseno.txt", "w") as file:
+        file.write("Miyu~Hello\n")
+        file.write("Ohoo~Yes\n")
+        file.write("Qayno~No\n")
+
+def load_file(Luiseno):
+    words = {}
+
+    with open (Luiseno , 'r') as file:
+        for line in file:
+            line = line.strip()
+            luiseno , english = line.split("~")
+            words[luiseno] = english
+    return words
+
+
 
 
 ##########################################

3814ef38ab04125d005075a48e047b04f566047c
Michael J Piper <michaelpiper444@gmail.com>
2026-04-28 17:33:25 +0000

    Final_UD3

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ import random
 ##########################################
 # <replace this line with function definitions, each needs a description>
 def create_file():
+    ''''Creates file with english and luiseno vocab'''
     with open("Luiseno.txt", "w") as file:
         file.write("Miyu~Hello\n")
         file.write("Ohoo~Yes\n")
@@ -28,7 +29,7 @@ def create_file():
 
 def load_file(Luiseno):
     words = {}
-
+    '''loads luiseno and english voacb into dictionary'''
     with open (Luiseno , 'r') as file:
         for line in file:
             line = line.strip()
@@ -37,7 +38,7 @@ def load_file(Luiseno):
     return words
 
 
-
+#dictionary was chosen for the key-value relationship between english and luiseno words
 
 ##########################################
 # MAIN PROGRAM:
@@ -53,14 +54,17 @@ def main():
         print("2. Exit")
 
         user_input = input("Please enter your choice")
-        if user_input == "1." or user_input == user_input.startswith("t"):
-            load_file(Luiseno)
-        elif user_input == user_input.startswith("2") or user_input.startswith("e"):
+        if user_input == "1" or user_input.startswith("t"):
+            words = load_file(file_name)
+            for luiseno, english in words.items():
+                print(luiseno, "=", english)
+        elif user_input.startswith("2") or user_input.startswith("e"):
             print("Thanks for checking out the program.")
+            break
         else:
             print("Invalid input. Please try again.")
 
-if __name__ == main():
+if __name__ == "__main__":
     main()
 
 

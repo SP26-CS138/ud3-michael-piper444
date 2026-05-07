03eb632ee9226f3a454061e7bf512a966d2cd78b
Michael J Piper <michaelpiper444@gmail.com>
2026-05-07 04:00:13 +0000

    updated_and_improved

--- main.py
+++ main.py
@@ -36,7 +36,16 @@ def load_file(Luiseno):
             luiseno , english = line.split("~")
             words[luiseno] = english
     return words
+def quiz_user(words):
+    luiseno_word = random.choice(list(words.keys()))
+    answer = input(f"What is the English meaning of '{luiseno_word}?")
+    if answer.lower() == words[luiseno_word].lower():
+        print("Correct!")
+    else:
+        print("Incorrect.")
+        print("The correct answer is:", words[luiseno_word])
 
+    
 
 #dictionary was chosen for the key-value relationship between english and luiseno words
 
@@ -47,18 +56,22 @@ def load_file(Luiseno):
 def main():
     file_name = "Luiseno.txt"
     create_file()
-    load_file(file_name)
     while True:
         print("Welcome to menu: ")
         print("1. See words")
-        print("2. Exit")
+        print("2. Quiz yourself")
+        print("3. Exit")
 
         user_input = input("Please enter your choice")
-        if user_input == "1" or user_input.startswith("t"):
+        if user_input == "1" or user_input.startswith("s"):
             words = load_file(file_name)
             for luiseno, english in words.items():
                 print(luiseno, "=", english)
-        elif user_input.startswith("2") or user_input.startswith("e"):
+        elif user_input == "2" or user_input.startswith("q"):
+            words= load_file(file_name)
+            quiz_user(words)
+            
+        elif user_input.startswith("3") or user_input.startswith("e"):
             print("Thanks for checking out the program.")
             break
         else:
